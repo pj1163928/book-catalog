c96704d5bbc0d2f8a31bc52b6522d662debdd86a
Paul Janota <pwjanota1@buffs.wtamu.edu>
2026-09-07 07:05:42 -0500

    Defined Publisher Book and Review models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    publisher = models.ForeignKey(
+        Publisher,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
+
+class Review(models.Model):
+    body = models.TextField()
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+
+    def __str__(self):
+        return f"Review of {self.book.title}"
