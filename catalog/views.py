f9bf20a95b8f5d530e87f3038d89fab60302e4a1
Paul Janota <pwjanota1@buffs.wtamu.edu>
2026-09-07 09:34:36 -0500

    Created three catalog pages: Books, Publishers and Reviews

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book, Publisher, Review
 
 # Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+    context_object_name = "books"
+
+class PublisherListView(ListView):
+    model = Publisher
+    template_name = "publisher_list.html"
+    context_object_name = "publishers"
+
+class ReviewListView(ListView):
+    model = Review
+    template_name = "review_list.html"
+    context_object_name = "reviews"
